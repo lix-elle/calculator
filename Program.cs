5d73b0237a72fa487e73a19d2c045aff17b59c01
Lynsy <lynsymariesoronio05@gmail.com>
2026-02-07 10:26:00 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,59 @@
-﻿//write your console display in here
+using System;
+
+public class Program
+{
+    static void Main()
+    {
+        while(true)
+        {
+            Console.WriteLine("Enter first number: ");
+            int num1;
+            while (!int.TryParse(Console.ReadLine(), out num1))
+            {
+                Console.WriteLine("Please enter a valid whole number.");
+                Console.WriteLine("Enter first number: ");
+            }
+            
+            Console.WriteLine("Enter second number: ");
+            int num2;
+            while (!int.TryParse(Console.ReadLine(), out num2))
+            {
+                Console.WriteLine("Please enter a valid whole number.");
+                Console.Writeline("Enter second number: ");
+            }
+            
+            
+            string operation;
+            while (true)
+            {
+                Console.WriteLine("Choose operation (+, -, *, /, %, =): ");
+                operation = Console.ReadLine();
+
+                if (operation == "=")
+                {
+                    Console.WriteLine("Exiting the program.");
+                    return;
+                }
+
+                if (operation == "+" || operation == "-" || operation == "*" || operation == "/" || operation == "%")
+                {
+                    break;
+                }
+
+                Console.WriteLine("Incorrect Operation Used, please try again.");
+            }
+
+            double? result = Calculator.PerformOperation(num1, num2, operation);
+
+            if (result == null)
+            {
+                continue;
+            }
+
+            Console.WriteLine("Result: " + result);
+
+            Console.WriteLine();
+
+        }
+    }
+}
